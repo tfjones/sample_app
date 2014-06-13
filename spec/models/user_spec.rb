87bdf5763d805b9f60dfba2d9c471d9f7ff20619
Timothy Jones <timothy_f_jones@hotmail.com>
2014-06-13 14:24:58 +1000

    finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@ describe User do
   it { should respond_to(:password_digest)}
   it { should respond_to(:password)}
   it { should respond_to(:password_confirmation)}
+  it { should respond_to(:remember_token) }
   it {should respond_to(:authenticate)}
   it { should be_valid}
   
@@ -89,8 +90,12 @@ describe User do
     end
   end
 
-  describe "user email should be converted to lower case" do
- 
+  describe "user email should be converted to lower case" do 
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
 
